f971a1210a628ba33ffd81ea12c253e5afecf73b
Nauman khan <muhammmadnauman992001647@gmail.com>
2024-09-22 23:16:14 +0500

    Login page template integrated

---
+++ E-Commerce-Website/Controllers/AdminController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace E_Commerce_Website.Controllers
+{
+    public class AdminController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Login() {
+            return View();
+        }
+    }
+}
